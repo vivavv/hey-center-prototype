e55e588ab1972baf9f4ab04c56081a17f9220655
Viviana <vivianavrc27@gmail.com>
2020-10-07 09:01:01 -0400

    dark theme colors

--- src/themes/AppTheme/index.vue
+++ src/themes/AppTheme/index.vue
@@ -14,24 +14,49 @@ export default {
 </script>
 <style lang="scss" scoped>
 .light {
-  --background-color: white;
+  --background-color: #ffffff;
   --on-background-color: #222;
   --primary-color: #1d2640;
   --secondary-color: #04133f;
   --gray: #707070;
-  --icons: white;
+  --light-gray: #b6b6b6;
+  --icons: #ffffff;
   --send-icon: #00bfff;
   --active-appbar: #6cc8d6;
   --active-icons: #b6b6b6;
-  --chat-border: #ededed;
+  --search-box: #ffffff;
+  --search-box-border: #2a334e;
+  --chat-border: #020202;
   --chat-bar: rgba(211, 211, 211, 0.2);
   --chat-item-active: rgba(211, 211, 211, 0.35);
+  --chat-background: #ffffff;
   --chat-status: rgba(112, 112, 112, 0.2);
   --chat-bubble: #f1f1f1;
+  --scroll-bar-color: #f5f5f5;
+  --scroll-bar-border: #f5f5f5;
+  --scroll-bar: #e0e0e0;
 }
 
 .dark {
   --background-color: #222;
   --on-background-color: white;
+  --primary-color: #0f0e14;
+  --secondary-color: #04133f;
+  --gray: #b6b6b6;
+  --light-gray: #d3d3d3;
+  --icons: #b6b6b6;
+  --send-icon: #00bfff;
+  --active-appbar: #e8e8e8;
+  --active-icons: #e8e8e8;
+  --search-box: #2b2c30;
+  --chat-border: #424242;
+  --chat-bar: rgba(211, 211, 211, 0.2);
+  --chat-item-active: rgba(211, 211, 211, 0.35);
+  --chat-background: #2b2b2d;
+  --chat-status: rgba(112, 112, 112, 0.2);
+  --chat-bubble: #4f4f4f;
+  --scroll-bar-color: #3e3e40;
+  --scroll-bar-border: #0f0e14;
+  --scroll-bar: #0f0e14;
 }
 </style>
